3a57ef5fb654d26439a106f678665ed008317434
yashshrivastav22 <yashshrivastav55@gmail.com>
2024-04-28 12:30:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 # AWS Configuration
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
-AWS_REGION = 'us-east-1'
+AWS_REGION = ''
 
 # Initialize AWS Rekognition client
 rekognition = boto3.client('rekognition', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
